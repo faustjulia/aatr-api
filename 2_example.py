1179fc5107ac14dd556484ce6b39960528143f4f
Yuliia Martynenko <julia.faust1103@gmail.com>
2021-09-22 15:35:13 -0700

    Once again updated survey

--- 2_example.py
+++ 2_example.py
@@ -9,43 +9,64 @@ from typing import Dict, List
 class Survey:
 
     def __init__(self,
-                 Q1_answer: str,
-                 # Q1_question: str = "What type of activity you enjoy the most?"
+                 Q1_answer: str
                  ):
 
         self.Q1_answer = Q1_answer
-        # self.Q1_question = Q1_question
-
-    def question(self):
-
-        questions: Dict[str, List[str]] = {
-            'Q1': [
-                'No activity',
-                'Aerobics',
-                'Balance',
-                'Flexibility',
-                'Strength'
-            ]
-        }
 
 
-        answers: Dict = {
-            questions['Q1'][0]:'Lorem ipsum 1. Question - ANSWER_1 '
+    def question(self,
+                 question: str) -> str:
+
+
+        if question == "What type of activity you enjoy the most?":
+
+            answers: Dict[str, str] = {
+                'No activity': 'Lorem ipsum 1. Question - ANSWER_1 '
                                'dolor sit amet, consectetur adipiscing elit.',
-            questions['Q1'][1]: 'Lorem ipsum 1. Question - ANSWER_2 '
-                                'Ut et augue id leo egestas interdum in eu lectus.',
-            questions['Q1'][2]: 'Lorem ipsum 1. Question - ANSWER_3 '
-                                'Nullam auctor odio vehicula',
-            questions['Q1'][3]: 'Lorem ipsum 1. Question - ANSWER_4 '
-                                'posuere elit in, ullamcorper lectus',
-            questions['Q1'][4]: 'Lorem ipsum 1. Question - ANSWER_5 '
-                                'Mauris pharetra dapibus congue.'
-        }
+                'Aerobics': 'Lorem ipsum 1. Question - ANSWER_2 '
+                            'Ut et augue id leo egestas interdum in eu lectus.',
+                'Balance': 'Lorem ipsum 1. Question - ANSWER_3 '
+                           'Nullam auctor odio vehicula',
+                'Flexibility': 'Lorem ipsum 1. Question - ANSWER_4 '
+                               'posuere elit in, ullamcorper lectus',
+                'Strength': 'Lorem ipsum 1. Question - ANSWER_5 '
+                            'Mauris pharetra dapibus congue.'
+            }
+
+        return answers[self.Q1_answer]
+
+survey = Survey(Q1_answer='No Activity').question(question="What type of activity you enjoy the most?")
+print(survey)
 
         # try:
         #     return topics[topic]
         # except KeyError:
         #     return f'Sorry, I don\'t know what you\'re talking about'
+
+        # questions: Dict[str, List[str]] = {
+        #     'Q1': [
+        #         'No activity',
+        #         'Aerobics',
+        #         'Balance',
+        #         'Flexibility',
+        #         'Strength'
+        #     ]
+        # }
+        #
+        #
+        # answers: Dict[str, str] = {
+        #     questions['Q1'][0]:'Lorem ipsum 1. Question - ANSWER_1 '
+        #                        'dolor sit amet, consectetur adipiscing elit.',
+        #     questions['Q1'][1]: 'Lorem ipsum 1. Question - ANSWER_2 '
+        #                         'Ut et augue id leo egestas interdum in eu lectus.',
+        #     questions['Q1'][2]: 'Lorem ipsum 1. Question - ANSWER_3 '
+        #                         'Nullam auctor odio vehicula',
+        #     questions['Q1'][3]: 'Lorem ipsum 1. Question - ANSWER_4 '
+        #                         'posuere elit in, ullamcorper lectus',
+        #     questions['Q1'][4]: 'Lorem ipsum 1. Question - ANSWER_5 '
+        #                         'Mauris pharetra dapibus congue.'
+        # }
 
         # if self.Q1_answer == questions['Q1'][0]:
         #     return 'Lorem ipsum 1. Question - ANSWER_1 dolor sit amet, consectetur adipiscing elit.'
@@ -59,8 +80,6 @@ class Survey:
         #     return 'Lorem ipsum 1. Question - ANSWER_5 Mauris pharetra dapibus congue.'
 
 
-survey = Survey(Q1_answer='No Activity').question()
-print(survey)
 
 
 
