c8ddb766cf65d7ee89469492a46dc079d0bcc251
Yuliia Martynenko <julia.faust1103@gmail.com>
2021-10-24 13:39:09 -0700

    After GitHub feedback

--- products/base.py
+++ products/base.py
@@ -1,175 +1,9 @@
-res_data = {'error': False, 'current_page': 1, 'next_page': 2, 'results': [
-    {'asin': 'B08ZSTH3Z3',
-     'title': 'NEEKFOX Water Bottle, 73OZ/0.6 Gallon Motivational Large Water Bottle with Straw & Time Marker, Leakproof BPA Free Big Water Jug, Wide Mouth Portable Water Bottle for Fitness Outdoor Sports',
-     'image': 'https://m.media-amazon.com/images/I/71kaCxOAdFS._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B08ZSTH3Z3/?psc=1',
-     'prices': {'current_price': 14.99, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 294, 'stars': 4.3}, 'prime': True,
-     'sponsored': False}, {'asin': 'B08BNH7TW1',
-                           'title': 'Venture Pal Large 1 Gallon Motivational Water Bottle with 2 Lids (Chug and Straw), Leakproof BPA Free Tritan Sports Water Jug with Time Marker to Ensure You Drink Enough Water Throughout The Day',
-                           'image': 'https://m.media-amazon.com/images/I/71b90xUDIHL._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B08BNH7TW1/?psc=1',
-                           'prices': {'current_price': 20.99,
-                                      'previous_price': -1.0, 'currency': '$'},
-                           'reviews': {'total_reviews': 17739, 'stars': 4.7},
-                           'prime': True, 'sponsored': True},
-    {'asin': 'B00WH2NKRO',
-     'title': 'Pogo BPA-Free Plastic Water Bottle with Chug Lid',
-     'image': 'https://m.media-amazon.com/images/I/71Jf8gGTbSL._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B00WH2NKRO/?psc=1',
-     'prices': {'current_price': 8.99, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 30994, 'stars': 4.7}, 'prime': True,
-     'sponsored': False}, {'asin': 'B089RGRCHP',
-                           'title': 'EYQ 32 oz Water Bottle with Time Marker, Carry Strap, Leak-Proof Tritan BPA-Free, Ensure You Drink Enough Water for Fitness, Gym, Camping, Outdoor Sports',
-                           'image': 'https://m.media-amazon.com/images/I/61GyrQ5m91L._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B089RGRCHP/?psc=1',
-                           'prices': {'current_price': 14.99,
-                                      'previous_price': -1.0, 'currency': '$'},
-                           'reviews': {'total_reviews': 5268, 'stars': 4.7},
-                           'prime': True, 'sponsored': False},
-    {'asin': 'B07Y8BW5QK',
-     'title': 'Iron Flask Sports Water Bottle - 64 Oz, 3 Lids (Straw Lid),Vacuum Insulated Stainless Steel, Double Walled, Thermo Mug, Metal Canteen',
-     'image': 'https://m.media-amazon.com/images/I/71RnrivGc2L._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B07Y8BW5QK/?psc=1',
-     'prices': {'current_price': 32.95, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 2873, 'stars': 4.9}, 'prime': True,
-     'sponsored': False}, {'asin': 'B07MGKJGP3',
-                           'title': 'Simple Modern Summit Insulated Water Bottle with Straw Lid 1 Liter Reusable Wide Mouth Stainless Steel Flask Thermos, 32oz (945ml), -Riptide',
-                           'image': 'https://m.media-amazon.com/images/I/61-Ovi2RBjL._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B07MGKJGP3/?psc=1',
-                           'prices': {'current_price': 21.99,
-                                      'previous_price': -1.0, 'currency': '$'},
-                           'reviews': {'total_reviews': 34107, 'stars': 4.8},
-                           'prime': True, 'sponsored': False},
-    {'asin': 'B07F6TKTBW',
-     'title': 'Thermoflask Double Stainless Steel Insulated Water Bottle, 40 oz, White',
-     'image': 'https://m.media-amazon.com/images/I/71dDzLe-WzL._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B07F6TKTBW/?psc=1',
-     'prices': {'current_price': 24.99, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 30175, 'stars': 4.8}, 'prime': True,
-     'sponsored': False}, {'asin': 'B096R85HG3',
-                           'title': '32oz Water Bottle with Time Marker & Straw, Dishwasher Safe, Leakproof, Safety Lock, No BPA, Reusable Water Jug to Ensure You Drink Enough Water Daily for Fitness, Gym, Outdoor Sports',
-                           'image': 'https://m.media-amazon.com/images/I/71C4JDZjVrL._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B096R85HG3/?psc=1',
-                           'prices': {'current_price': 10.99,
-                                      'previous_price': 20.0, 'currency': '$'},
-                           'reviews': {'total_reviews': 162, 'stars': 4.7},
-                           'prime': True, 'sponsored': False},
-    {'asin': 'B00TNIK6P4', 'title': 'Nalgene Wide Mouth Water Bottle',
-     'image': 'https://m.media-amazon.com/images/I/71yXE2dXkLL._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B00TNIK6P4/?psc=1',
-     'prices': {'current_price': 11.99, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 46633, 'stars': 4.9}, 'prime': True,
-     'sponsored': False}, {'asin': 'B08B1VTTB9',
-                           'title': 'Triple-Insulated Stainless Steel Water Bottle with Straw Lid - Flip-Top Lid - Wide-Mouth Cap (25 Oz) Insulated Water Bottles, Keeps Hot and Cold - Sports Canteen Water Bottle Great for Hiking & Biking',
-                           'image': 'https://m.media-amazon.com/images/I/81HHI2mPzEL._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B08B1VTTB9/?psc=1',
-                           'prices': {'current_price': 19.99,
-                                      'previous_price': -1.0, 'currency': '$'},
-                           'reviews': {'total_reviews': 10073, 'stars': 4.8},
-                           'prime': True, 'sponsored': False},
-    {'asin': 'B0878R9YQN',
-     'title': 'Giotto Large 1 Gallon Motivational Water Bottle with Paracord Handle & Removable Straw - Leakproof Tritan BPA Free Fitness Sports Water Jug with Time Marker to Ensure You Drink Enough Water Daily',
-     'image': 'https://m.media-amazon.com/images/I/71iygo7QTkL._AC_UL320_.jpg',
-     'full_link': 'https://www.amazon.com/dp/B0878R9YQN/?psc=1',
-     'prices': {'current_price': 20.99, 'previous_price': -1.0,
-                'currency': '$'},
-     'reviews': {'total_reviews': 20546, 'stars': 4.6}, 'prime': True,
-     'sponsored': False}, {'asin': 'B01KXHH42G',
-                           'title': 'Hydro Flask Water Bottle - Stainless Steel, Reusable, Vacuum Insulated with Standard Mouth Flex Lid',
-                           'image': 'https://m.media-amazon.com/images/I/61rjkR4gPiS._AC_UL320_.jpg',
-                           'full_link': 'https://www.amazon.com/dp/B01KXHH42G/?psc=1',
-                           'prices': {'current_price': 27.39,
-                                      'previous_price': 29.95,
-                                      'currency': '$'},
-                           'reviews': {'total_reviews': 23701, 'stars': 4.8},
-                           'prime': True, 'sponsored': False},
-]}
-
 from typing import Dict, List
 
 import requests
 
+from products.response import res_data
 
-# Hiking shoes = 55
-# Sunscreen = 60
-# Beach towel = 60
-# Sun glasses = 59
-# Water bottle = 60
-
-# data = response.json()
-# print(data)
-#
-# print(response.json()['results'][0].get('title'))
-# print(response.json()['results'][0].get('image'))
-# print(response.json()['results'][0].get('full_link'))
-# print(response.json()['results'][0].get('prices'))
-
-# TODO: Exercise 1: A proper request following all the previous instructions
-# 1. Add variable type to the 'response'
-# 2. Instead of using separate 'url' variable,
-#    move 'https://amazon-products1.p.rapidapi.com/search' in .get(url=...)
-# 3. Instead of using separate 'headers' variable,
-#    move {... headers_content ...} in .get(headers=...)
-# 4. Instead of using separate 'querystring' variable,
-#    move {... querystring_content ...} in .get(params=...)
-# 5. Choose one either single or double quotes,
-#    and stick with them - replace all of them with one type.
-
-# TODO: Exercise 2: Create a 'ProductRequest' class
-# 1. Create a new 'ProductRequest' class blueprint
-# 2. Create a new method called 'def _request(self) -> requests.Response:'
-# 3. Move the content of 'Exercise 1' to the new '_request' method (return the response)
-
-
-# TODO: Exercise 3: Move parts that don't change to the __init__
-# 1. Create a new 'self.url' variable in '__init__' method (and use it in code)
-# 2. Create a new 'self.headers' variable in '__init__' method (and use it in code)
-
-# TODO: Exercise 4: Add 'timeout' (in case we can't reach the API url)
-# 1. Create a separate method called '.get()' in 'ProductRequest' class
-# 2. Adjust '_request' method to use 'getattr()' and '**kwargs' (see the video)
-# 3. Add new variable called 'self.timeout' to __init__ method (see the video)
-# 4. Update 'kwargs' with 'self.timeout' in '_request' method (see the video)
-# 5. Adjust '.get()' method to use '_request' method (see the video)
-
-# TODO: Exercise 5: Data extraction for the first product
-# 1. Use 'ProductRequest' to create a new request
-# 2. Assign the result of .get() method execution to a new variable called 'res'
-# 3. Create a new variable 'res_data: Dict = res.json()'
-# 4. Get the first product from the 'results' key in 'res_data'
-#    and assign it to a new variable called 'product'
-# 5. Find out how to access product fields:
-#    'title', 'image', 'full_link', 'current_price', 'currency'
-#    using 'product' variable
-# 6. Create a new empty 'List' variable called 'products'
-# 7. Append a new item
-#    (Dict with product 'title', 'image', 'full_link', 'current_price', 'currency')
-#    to 'products'
-
-# TODO: Exercise 6: Create 'get_products' function
-# 1. Create a new function called 'get_products'
-# 2. 'get_products' function takes 1 argument 'res_data'
-# 3. The goal of the function is to return a list of 5 products
-# 4. Each product should be appended to the 'products' list
-#    as a Dict with with product 'title', 'image', 'full_link', 'current_price', 'currency'
-# 5. Use 'for loop' to with 'break' statement to end the 'for loop' once you have collected 5 products
-# 6. Use if statement to only add products with current_price higher than -1.0 (float)
-# 7. The function should return a list of products (add a return type to the function)
-
-# TODO: Exercise 7: Create 'Products' class
-# 1. Move 'get_products()' function to the 'Products' class
-# 2. Rename 'get_products()' function to just 'get()' method
-# 3. Create __init__ method with two arguments: 
-#    'res_data' and 'amount'
-# 4. Create two variables in __init__ method:
-#    'self.data' (for 'res_data') and 'self.amount' for 'amount'
-# 5. Use 'self.data' and  'self.amount' in 'get()' method
 
 class ProductRequest:
 
@@ -181,21 +15,22 @@ class ProductRequest:
         }
         self.timeout = 20
 
-    def _request(self,
-                 method: str,
-                 **kwargs) -> requests.Response:
+    def _request(
+        self,
+        method: str,
+        **kwargs
+    ) -> requests.Response:
         kwargs.update({
             'timeout': self.timeout
         })
         return getattr(requests, method)(**kwargs)
 
-    def get(self,
-            params: Dict) -> requests.Response:
+    def get(
+        self,
+        params: Dict
+    ) -> requests.Response:
         return self._request(
-            method='get',
-            url=self.url,
-            headers=self.headers,
-            params=params
+            method='get', url=self.url, headers=self.headers, params=params
         )
 
 
@@ -205,23 +40,29 @@ data: Dict = {
     'page': '1'
 }
 
+DEBUG = True
 
-# request = ProductRequest()
-# res: requests.Response = request.get(params=data)
-# res_data: Dict = res.json()
-# print(res)
-# print(res_data)
+if not DEBUG:
+    request = ProductRequest()
+    res: requests.Response = request.get(params=data)
+    res_data: Dict = res.json()
+    print(res)
+
+print(res_data)
+
 
 class Products:
 
-    def __init__(self,
-                 data: Dict,
-                 amount: int):
+    def __init__(
+        self,
+        data: Dict,
+        amount: int
+    ):
 
         self.data = data
         self.amount = amount
 
-    def get(self):
+    def get(self) -> List:
         products: List = []
 
         for product in self.data['results']:
