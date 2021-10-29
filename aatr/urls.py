3cedbd6e68236b337c453912c7f7110a699ea70a
Yuliia Martynenko <julia.faust1103@gmail.com>
2021-10-29 13:03:24 -0700

    Setting up Django

--- aatr/urls.py
+++ aatr/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from aatr.views import get_sample
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sample/', get_sample, name='get_sample'),
 ]
