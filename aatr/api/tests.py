162ab2cd4521a11b85c14ba5bed48d456546da47
Yuliia Martynenko <julia.faust1103@gmail.com>
2022-02-08 11:08:56 -0800

    Finalized survey

--- aatr/api/tests.py
+++ aatr/api/tests.py
@@ -1,11 +1,291 @@
 import json
+import typing
+from unittest.mock import patch, Mock
 
+import requests
 from django.conf import settings
 from django.test import TestCase
+from requests.exceptions import HTTPError
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 
 from aatr.api.models import User
+
+
+class TestSignUpEndpoint(TestCase):
+
+    def test_signup_success(self):
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'jules@gmail.com',
+                'password': 'eegwrgewgwe',
+                'terms': True
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            201
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                'detail': 'success'
+            }
+        )
+
+    def test_invalid_email(self):
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'invalidemail',
+                'password': 'eegwrgewgwe',
+                'terms': True
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            400
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "email": [
+                    "Enter a valid email address."
+                ]
+            }
+        )
+
+    def test_password_less_than_min_length(self):
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'jules@gmail.com',
+                'password': '123',
+                'terms': True
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            400
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "password": [
+                    "Ensure this field has at least 8 characters."
+                ]
+            }
+        )
+
+    def test_password_more_than_max_length(self):
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'jules@gmail.com',
+                'password': '123jwefwegfouheifhbvuhguw',
+                'terms': True
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            400
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "password": [
+                    "Ensure this field has no more than 16 characters."
+                ]
+            }
+
+        )
+
+    def test_unaccepted_terms_checkbox(self):
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'jules@gmail.com',
+                'password': '123jwefwegfow',
+                'terms': False
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            400
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "terms": [
+                    "You must accept terms and conditions."
+                ]
+            }
+
+        )
+
+    def test_email_already_exists(self):
+        user: User = User.objects.create_user(
+            email='jules@gmail.com'
+        )
+
+        client: APIClient = APIClient()
+
+        res: Response = client.post(
+            path='/api/signup/',
+            data=json.dumps({
+                'name': 'Yuliia Martynenko',
+                'email': 'jules@gmail.com',
+                'password': '123jwefwegfow',
+                'terms': True
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            400
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "email": [
+                    "This email address is already being used."
+                ]
+            }
+        )
+
+    def test_failed_to_send_email(self):
+        client: APIClient = APIClient()
+
+        mocked_response = patch.object(
+            target=requests,
+            attribute='post',
+            side_effect=HTTPError
+        )
+
+        with mocked_response:
+            res: Response = client.post(
+                path='/api/signup/',
+                data=json.dumps({
+                    'name': 'Yuliia Martynenko',
+                    'email': 'jules@gmail.com',
+                    'password': '123jwefwegfow',
+                    'terms': True
+                }),
+                content_type='application/json'
+            )
+
+        self.assertEqual(
+            res.status_code,
+            503
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                "detail": "Service Unavailable"
+            }
+        )
+
+        was_user_created: bool = User.objects.filter(
+            name='Yuliia Martynenko',
+            email='jules@gmail.com',
+            password='123jwefwegfow',
+            terms=True
+        ).exists()
+
+        self.assertEqual(
+            was_user_created,
+            False
+        )
+
+    def test_successful_signup(self):
+        client: APIClient = APIClient()
+
+        mocked_response = patch.object(
+            target=requests,
+            attribute='post',
+            side_effect=[
+                Mock(
+                    **{
+                        'status_code': 200,
+                        'json.return_value': {
+                            'id': '<20220113195830.0f9bc6de9b894717@yuliiamartynenko.com>',
+                            'message': 'Queued. Thank you.'}
+
+                    }
+                )
+            ]
+        )
+
+        sign_up_data: typing.Dict = {
+            'name': 'Yuliia Martynenko',
+            'email': 'julia.faust1103@gmail.com',
+            'password': '123jwefwegfow',
+            'terms': True
+        }
+
+        with mocked_response:
+            res: Response = client.post(
+                path='/api/signup/',
+                data=json.dumps(sign_up_data),
+                content_type='application/json'
+            )
+
+        self.assertEqual(
+            res.status_code,
+            201
+        )
+
+        was_user_created: bool = User.objects.filter(
+            email=sign_up_data['email'],
+        ).exists()
+
+        self.assertEqual(
+            was_user_created,
+            True
+        )
+
+        self.assertEqual(
+            res.json(),
+            {
+                'id': '<20220113195830.0f9bc6de9b894717@yuliiamartynenko.com>',
+                'message': 'Queued. Thank you.'
+            }
+
+        )
 
 
 class TestSignin(TestCase):
--- aatr/urls.py
+++ aatr/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from aatr.views import signin
-from aatr.views import get_sample, signup_endpoint
+from aatr.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signin/', signin),
-    path('sample/', get_sample),
-    path('api/signup/', signup_endpoint)
+    path('api/signup/', signup)
 ]
